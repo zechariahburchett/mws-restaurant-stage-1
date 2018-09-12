54ffbd90be1abb033b1832d6b263bcb2d3456176
Zechariah Burchett <zechariah.burchett@gmail.com>
2018-09-12 12:38:08 -0400

    added tab indexs to main app page

--- js/main.js
+++ js/main.js
@@ -176,6 +176,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
+  more.setAttribute('tabIndex', '3');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
